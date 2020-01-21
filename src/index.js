b0bf39925b22af0798de3ad795434330a5caac93
Manu Schiller <manuel.schiller@sevenval.com>
2020-01-21 10:47:17 +0100

    Add more shorthand props

--- src/index.js
+++ src/index.js
@@ -28,6 +28,42 @@ const t = "top";
 const r = "right";
 const b = "bottom";
 const l = "left";
+const w = "width";
+const h = "height";
+const d = "display";
+const min = "min";
+const max = "max";
+const brd = "border";
+const bx = "box";
+const shd = "shadow";
+const grid = "grid";
+const ap = "ap";
+const gap = `g${ap}`;
+const Gap = `G${ap}`;
+const row = "row";
+const col = "col";
+const color = `${col}or`;
+const column = `${col}umn`;
+const flex = "flex";
+const wrap = "wrap";
+const dir = "dir";
+const direction = `${dir}ection`;
+const template = "template";
+const align = "align";
+const justify = "justify";
+const content = "content";
+const items = "items";
+const area = "area";
+const auto = "auto";
+const flow = "flow";
+const bg = "bg";
+const background = "background";
+const image = "image";
+const size = "size";
+const pos = "pos";
+const position = "position";
+const repeat = "repeat";
+const attachment = "attachment";
 
 export const shortHandAttributes = new Map([
   ["m", [m]],
@@ -44,8 +80,57 @@ export const shortHandAttributes = new Map([
   ["pl", [`${p}-${l}`]],
   ["px", [`${p}-${l}`, `${p}-${r}`]],
   ["py", [`${p}-${t}`, `${p}-${b}`]],
-  ["bg", ["background-color"]],
-  ["size", ["width", "height"]]
+  [bg, [`${background}-${color}`]],
+  [`${bg}${image}`, [`${background}-${image}`]],
+  [`${bg}-${image}`, [`${background}-${image}`]],
+  [`${bg}${size}`, [`${background}-${size}`]],
+  [`${bg}-${size}`, [`${background}-${size}`]],
+  [`${bg}${pos}`, [`${background}-${position}`]],
+  [`${bg}-${pos}`, [`${background}-${position}`]],
+  [`${bg}${repeat}`, [`${background}-${repeat}`]],
+  [`${bg}-${repeat}`, [`${background}-${repeat}`]],
+  [`${bg}-${attachment}`, [`${background}-${attachment}`]],
+  [`${bg}${attachment}`, [`${background}-${attachment}`]],
+  ["w", [w]],
+  ["h", [h]],
+  [size, [w, h]],
+  ["d", [d]],
+  [`${min}w`, [`${min}-${w}`]],
+  [`${min}-w`, [`${min}-${w}`]],
+  [`${max}w`, [`${max}-${w}`]],
+  [`${max}-w`, [`${max}-${w}`]],
+  [`${min}h`, [`${min}-${h}`]],
+  [`${min}-h`, [`${min}-${h}`]],
+  [`${max}h`, [`${max}-${h}`]],
+  [`${max}-h`, [`${max}-${h}`]],
+  ["bx", [`${brd}-${l}`, `${brd}-${r}`]],
+  ["by", [`${brd}-${t}`, `${brd}-${b}`]],
+  [shd, [`${bx}-${shd}`]],
+  [align, [`${align}-${items}`]],
+  [justify, [`${justify}-${content}`]],
+  [wrap, [`${flex}-${wrap}`]],
+  [`${flex}${dir}`, [`${flex}-${direction}`]],
+  [direction, [`${flex}-${direction}`]],
+  [gap, [`${grid}-${gap}`]],
+  [`${row}`, [`${grid}-${row}`]],
+  [`${row}${Gap}`, [`${grid}-${row}-${gap}`]],
+  [`${row}-${gap}`, [`${grid}-${row}-${gap}`]],
+  [`${column}`, [`${grid}-${col}`]],
+  [`${col}${Gap}`, [`${grid}-${column}-${gap}`]],
+  [`${col}-${gap}`, [`${grid}-${column}-${gap}`]],
+  [`${area}`, [`${grid}-${area}`]],
+  [`${auto}${flow}`, [`${grid}-${auto}-${flow}`]],
+  [`${auto}-${flow}`, [`${grid}-${auto}-${flow}`]],
+  [`${auto}${row}s`, [`${grid}-${auto}-${row}s`]],
+  [`${auto}-${row}s`, [`${grid}-${auto}-${row}s`]],
+  [`${auto}${column}s`, [`${grid}-${auto}-${column}s`]],
+  [`${auto}-${column}s`, [`${grid}-${auto}-${column}s`]],
+  [`${template}${row}s`, [`${grid}-${template}-${row}s`]],
+  [`${template}-${row}s`, [`${grid}-${template}-${row}s`]],
+  [`${template}${column}s`, [`${grid}-${template}-${col}s`]],
+  [`${template}-${column}s`, [`${grid}-${template}-${col}s`]],
+  [`${template}${area}s`, [`${grid}-${area}-${area}s`]],
+  [`${template}-${area}s`, [`${grid}-${area}-${area}s`]]
 ]);
 
 export function styled(node, props) {
@@ -60,7 +145,7 @@ export function styled(node, props) {
     let mediaQueries = [];
 
     for (let [name, value] of Object.entries(attributes)) {
-      name = shortHandAttributes.get(name) || [name];
+      name = shortHandAttributes.get(name.toLowerCase()) || [name];
       for (let cssProp of name) {
         // unresponsive definitions
         if (!Array.isArray(value)) {
